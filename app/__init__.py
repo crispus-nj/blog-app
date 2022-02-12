5b9bb92b630e5a30de174724c08330a975fe353e
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-12 19:04:10 +0300

    initialized SQLALCHEMY_TRACK_MODIFICATIONS to true in the config.py file. Created a database instance using flask-Sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .config import DevConfig
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'this'
-
-# 
 app.config.from_object(DevConfig)
+
+db = SQLAlchemy(app)
 
 from app import views
