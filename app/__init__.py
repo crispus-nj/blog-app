88b96d7aa47d04e212d4074b5ec8e41ffa6ea09d
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-12 18:48:44 +0300

    added class config to the config.py file. class Config was inherited by ProdConfig and DevConfig class which is passed to debug to True on develoment mode. The class is  import to the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'this'
+
+# 
+app.config.from_object(DevConfig)
 
 from app import views
