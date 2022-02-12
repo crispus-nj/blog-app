0d5fef1d0a2a059addfb187983cc6ba7c91ed320
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-12 19:07:14 +0300

    created user model and added 5 columns and a relationship to the table. The table will be related to the Post table

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db 
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(150))
+    post = db.relationship('Post', backref='author', lazy=True)
