a52c79b0386ed67747bc1b109bcaeb0ff48b6dea
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-15 19:05:59 +0300

    improve the app by adding a function to the init file, added app instance in the function then imported the function to manage.py for running the application

--- app/__init__.py
+++ app/__init__.py
@@ -5,26 +5,35 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_mail import Mail
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'this'
-app.config.from_object(DevConfig)
+db = SQLAlchemy()
+Migrate(db)
 
-db = SQLAlchemy(app)
-Migrate(app, db)
 # login configurations
 login_manger = LoginManager()
-login_manger.init_app(app)
+# login_manger.init_app()
 login_manger.login_view = 'login'
 login_manger.login_message_category = 'info'
 
 
-mail = Mail(app)
+mail = Mail()
 
-from app.users.views import users
-from app.main.views import main
-from app.posts.views import posts
+def create_app(config_class=DevConfig):
 
-app.register_blueprint(users)
-app.register_blueprint(main)
-app.register_blueprint(posts)
+    app = Flask(__name__)
+    app.config.from_object(DevConfig)
+    app.config['SECRET_KEY'] = 'this'
+
+    db.init_app(app)
+    login_manger.init_app(app)
+    mail.init_app(app)
+
+    from app.users.views import users
+    from app.main.views import main
+    from app.posts.views import posts
+
+    app.register_blueprint(users)
+    app.register_blueprint(main)
+    app.register_blueprint(posts)
+
+    return app
